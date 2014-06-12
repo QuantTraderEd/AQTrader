484d91575b26d39193525144771e27cf9a1067a6
QuantTraderEd <hyojkim79@gmail.com>
2014-06-12 16:06:40 +0900

    new thread file

---
+++ zerooptionviewer_thread.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jun 12 10:27:17 2014
+
+@author: MJWOO
+"""
+
+import zmq
+from PyQt4 import QtCore
+
+def cybos_convertor(strprice, floating):
+    value = round(float(strprice), floating)
+    strformat = '%.' + str(floating) + 'f'
+    strValue = strformat%value
+    return (strValue)
+    
+class OptionViewerThread(QtCore.QThread):
+    receiveData = QtCore.pyqtSignal(str)
+    def __init__(self,parent=None):
+        QtCore.QThread.__init__(self,parent)
+        self.msg = ''
+        self.mt_stop = False
+        self.mt_pause = False
+        self.mutex = QtCore.QMutex()
+        self.mt_pauseCondition = QtCore.QWaitCondition()        
+        
+        
+    def run(self):
+        context = zmq.Context()
+        self.socket = context.socket(zmq.SUB)
+        self.socket.connect("tcp://127.0.0.1:5500")
+        self.socket.setsockopt(zmq.SUBSCRIBE,"")  
+        
+        while True:
+            msg =  self.socket.recv()
+            self.receiveData.emit(msg)
+                                
+            self.mutex.lock()
+            if self.mt_stop: break    
+            self.mutex.unlock()
