7a923fd2e37bcf290707d86f2eb8eb24aca1baaf
QuantTraderEd <hyojkim79@gmail.com>
2014-07-15 14:56:35 +0900

    Update zerooptionviewer_executedlg.py
    add initOrder function

--- zerooptionviewer_executedlg.py
+++ zerooptionviewer_executedlg.py
@@ -35,25 +35,31 @@ class OptionViewerExecuteDlg(QtGui.QDialog):
         self.socket = context.socket(zmq.REQ)
         self.socket.connect("tcp://127.0.0.1:6000")
         pass
-        
-                
+    
+    def initOrder(self,buysell,shcode,price,qty):
+        self.ui.pushButtonBuySell.setChecked(buysell)
+        self.ui.lineEditShortCode.setText(shcode)
+        self.ui.doubleSpinBoxPrice.setValue(price)
+        self.ui.spinBoxQty.setValue(qty)
+        pass
         
     def onSend(self):
         buysell = self.ui.pushButtonBuySell.isChecked()
-        shname = self.ui.lineEditShortName.text()
+        shcode = self.ui.lineEditShortCode.text()
         price = self.ui.doubleSpinBoxPrice.value()
         qty = self.ui.spinBoxQty.value()
-        msg = str(buysell) + ',' + str(shname) + ',' + str(price) + ',' + str(qty)
+        msg = str(buysell) + ',' + str(shcode) + ',' + str(price) + ',' + str(qty)
         #self.socket.send(msg)
         #msg_in = self.socket.recv()
         print msg
         #print buysell,price,qty
         
+        
 if __name__ == "__main__":
     import sys    
     app = QtGui.QApplication(sys.argv)
     mydlg = OptionViewerExecuteDlg()
-    mydlg.ui.doubleSpinBoxPrice.setValue(1.25)
+    mydlg.initOrder(True,'201J8260',1.30,1)
     mydlg.show()
     app.exec_()  
         
