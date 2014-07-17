11ed6b50e6d25c1b03b3955fe5b1648f8de9f937
QuantTraderEd <hyojkim79@gmail.com>
2014-07-18 00:12:44 +0900

    update zerooptionviewer_executewidget - update popup position

--- zerooptionviewer_executewidget.py
+++ zerooptionviewer_executewidget.py
@@ -4,50 +4,88 @@ Created on Tue Jul 15 23:32:11 2014
 
 @author: assa
 """
-
-import sys
+import zmq
 from PyQt4 import QtCore, QtGui
 from ui_executewidget import Ui_Form
 
 
-class ExcuteWidget(QtGui.QWidget):
+class OptionViewerExecuteWidget(QtGui.QWidget):
     def __init__(self,parent = None, widget = None):
         QtGui.QWidget.__init__(self,parent)
+        self.initUI(widget)
+        self.initZMQ()
+        
+        
+    def initUI(self,widget=None):
         self.ui = Ui_Form()
         self.ui.setupUi(self)
-        
-        # adjust the margins or you will get an invisible, uninterended border
-        self.ui.horizontalLayout.setContentsMargins(0,0,0,0)
+        self.onToggled()
 
-        # need to set the layout
-        self.adjustSize()
+        self.ui.horizontalLayout.setContentsMargins(0,0,0,0)
+        self.resize(220,40)
         
-        # tag this widget as a popup
         self.setWindowFlags(QtCore.Qt.Popup)
         
-        # calculate the bottom right point from the parents rectangle
         point = widget.rect().bottomRight()
         
-        # map that point as a global position
         global_point = widget.mapToGlobal(point)
-        
-        # by default, a widget will be placed from its top-left corner, so
-        # we need to move it to the left based on the widgets width
         self.move(global_point - QtCore.QPoint(self.width(), 0))
         
-class Window(QtGui.QWidget):
-    def __init__(self):
-        QtGui.QWidget.__init__(self)
-        self.button = QtGui.QPushButton('Hit this button to show a popup', self)
-        self.button.clicked.connect(self.handleOpenDialog)
-        self.button.move(250, 50)
-        self.resize(600, 200)
+        self.ui.pushButtonSend.clicked.connect(self.onSend)
+        self.ui.radioButtonBuy.toggled.connect(self.onToggled)
+        pass
+    
+    def initZMQ(self):
+        context = zmq.Context()
+        self.socket = context.socket(zmq.REQ)
+        self.socket.connect("tcp://127.0.0.1:6000")
+        pass
+    
+    def initOrder(self,buysell=True,shcode='',price=0,qty=0):
+        print buysell
+        self.ui.radioButtonBuy.setChecked(buysell)
+        self.ui.radioButtonSell.setChecked(not buysell)
+        self.ui.lineEditShortCode.setText(shcode)
+        self.ui.doubleSpinBoxPrice.setValue(price)
+        self.ui.spinBoxQty.setValue(qty)
+        self.onToggled()
+        pass
         
-    def handleOpenDialog(self):
-        self.popup = ExcuteWidget(self, self.button)
-        self.popup.show()
+    def onSend(self):
+        buysell = self.ui.radioButtonBuy.isChecked()
+        shcode = self.ui.lineEditShortCode.text()
+        price = self.ui.doubleSpinBoxPrice.value()
+        qty = self.ui.spinBoxQty.value()
+        msg = str(buysell) + ',' + str(shcode) + ',' + str(price) + ',' + str(qty)
+        #self.socket.send(msg)
+        #msg_in = self.socket.recv()
+        print msg
+        #print buysell,price,qty
+        
+    def onToggled(self):
+        buysell = self.ui.radioButtonBuy.isChecked()
+        if buysell:
+            self.ui.pushButtonSend.setText('Buy')
+        elif not buysell:
+            self.ui.pushButtonSend.setText('Sell')
+        
+
         
 if __name__ == '__main__':
+    import sys
+    class Window(QtGui.QWidget):
+        def __init__(self):
+            QtGui.QWidget.__init__(self)
+            self.button = QtGui.QPushButton('Hit this button to show a popup', self)
+            self.button.clicked.connect(self.handleOpenDialog)
+            self.button.move(250, 50)
+            self.resize(600, 200)
+            
+        def handleOpenDialog(self):
+            self.popup = OptionViewerExecuteWidget(self, self.button)
+            self.popup.show()    
+            self.popup.initOrder(False,'201J8260',0.60,1)
+        
     app = QtGui.QApplication(sys.argv)
     win = Window()
     win.show()
