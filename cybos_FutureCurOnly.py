384dd4672207c1fa74639cbb4d66c403ecb744bf
QuantTraderEd <hyojkim79@gmail.com>
2014-08-28 14:16:45 +0900

    Update cybos_FutureCurOnly.py
    Change the count of data items

--- cybos_FutureCurOnly.py
+++ cybos_FutureCurOnly.py
@@ -17,7 +17,7 @@ class FutureCurOnly(Source):
         pass
     def OnSignal(self):
         self.data = []
-        for i in xrange(31): self.data.append(self.com.GetHeaderValue(i))
+        for i in xrange(34): self.data.append(self.com.GetHeaderValue(i))
         #for debug
         self.Notify()
         pass
