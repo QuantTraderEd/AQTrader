898ea74810a494337124f73d12100e8e11e0ff4c
QuantTraderEd <hyojkim79@gmail.com>
2014-09-03 18:22:31 +0900

    Update QtViewerC01.py
    Add update working order data part of DB (ExecuPrice, ExecuQty, unexecqty)

--- QtViewerC01.py
+++ QtViewerC01.py
@@ -9,6 +9,9 @@ import sqlite3 as lite
 from PyQt4 import QtCore
 from datetime import datetime
 
+def convert(strprice):
+    return '%.2f' %round(float(strprice),2)
+
 
 class QtViewerC01(QtCore.QObject):
     receive = QtCore.pyqtSignal()
@@ -45,9 +48,33 @@ class QtViewerC01(QtCore.QObject):
                 conn_db = lite.connect(self.dbname)
                 cursor_db = conn_db.cursor()
                 cursor_db.execute("""INSERT INTO OrderList(OrdNo,ExecNo,Time,BuySell,ShortCD,Price,Qty,ExecPrice,ExecQty,UnExecQty) 
-                                                VALUES(?, ?, ?, ? ,?, ?, ?, ?, ?, ?)""",orderitem)  
-#                cursor_db.execute("""Update OrderList Set UnExecQty=? 
-#                                                WHERE OrdNo=? and (BuySell = 'buy' or BuySell = 'sell') """, (unexecqty,ordno))
+                                                VALUES(?, ?, ?, ? ,?, ?, ?, ?, ?, ?)""",orderitem) 
+                                                
+                cursor_db.execute("""Select ExecPrice,ExecQty,UnExecQty From OrderList 
+                                    WHERE OrdNo = ? and ShortCD = ? and ChkReq is not null """,(str(ordno),str(shcode),))
+                rows = cursor_db.fetchall()                
+                unexecqty = 0
+                avgExecPrice = 0
+                avgExecQty = 0
+                if len(rows) == 1:
+                    if rows[0][0]:
+                        avgExecPrice = float(rows[0][0])
+                    if rows[0][1]:
+                        avgExecQty = int(rows[0][1])
+                    unexecqty = int(rows[0][2])
+                    avgExecPrice = (avgExecPrice * avgExecQty + float(execprice) * int(execqty)) / (avgExecQty + int(execqty))
+                    avgExecQty =  avgExecQty + int(execqty)    
+                    avgExecPrice = convert(avgExecPrice)
+                    
+                
+                if unexecqty > 0:
+                    cursor_db.execute("""Update OrderList Set ExecPrice=?, ExecQty=?, UnExecQty=? 
+                                                                    WHERE OrdNo=? and ShortCD = ? and (BuySell = 'buy' or BuySell = 'sell') 
+                                                                    and ChkReq is not null
+                                                                    """, 
+                                                                    (str(avgExecPrice), str(avgExecQty),str(unexecqty - int(execqty)),str(ordno),str(shcode),))
+                
+
                 conn_db.commit()
                 conn_db.close()
                 self.receive.emit()
