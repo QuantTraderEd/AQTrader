6c5ad73fd72196fc2a2b388469cacfda85ec29c4
QuantTraderEd <hyojkim79@gmail.com>
2014-09-22 18:18:46 +0900

    Update optionviewer_simplealgotrader2.py
    Add initDB function
    Add part of getting bid, ask price from DB

--- optionviewer_simplealgotrader2.py
+++ optionviewer_simplealgotrader2.py
@@ -7,9 +7,13 @@ Created on Fri Sep 12 09:01:06 2014
 
 import time
 import zmq
+import sqlite3 as lite
 from PyQt4 import QtCore, QtGui
 from zerooptionviewer_thread import OptionViewerThread
 from FeedCodeList import FeedCodeList
+
+def convert(strprice):
+    return '%.2f' %round(float(strprice),2)
 
 
 class SimpleAlgoTrader(QtGui.QWidget):
@@ -34,9 +38,16 @@ class SimpleAlgoTrader(QtGui.QWidget):
         self.counter = 0
         self.counter1 = 0
         self.callShCode = '201JA267'
-        self.putShCode = '301JA255'
+        self.putShCode = '301JA257'
         self.entry_counter1 = 0
         self.entry_counter2 = 0
+        pass
+
+    def initDB(self):
+        self.conn = lite.connect(":memory:")
+        self.cur = self.conn.cursor()
+        self.cur.execute("""CREATE TABLE options
+             (Time text, ShCode text, AskQty1 text, Ask1 text, Bid1 text, BidQty1 text)""")
         pass
         
     def initFeedCode(self):
@@ -79,14 +90,115 @@ class SimpleAlgoTrader(QtGui.QWidget):
         pass
     
     def onReceiveData(self,msg):
-        print msg
+        nowtime = datetime.now()
+        strnowtime = datetime.strftime(nowtime,'%H:%M:%S.%f')
+        lst = msg.split(',')
+        if lst[1] == 'cybos' and lst[2] == 'Q' and lst[3] == 'options':
+            shcode = str(lst[4])
+            ask1 = convert(lst[6])
+            bid1 = convert(lst[23])
+            askqty1 = str(lst[11])
+            bidqty1 = str(lst[28])
+
+            if shcode in [self.callShCode,self.putShCode]:
+                taqitem = (strnowtime,shcode,askqty1,ask1,bid1,bidqty1)
+                print lst[0],taqitem
+                self.cur.execute("""INSERT INTO TickData(Time,ShCode,AskQty1,Ask1,Bid1,BidQty1)
+                                                VALUES(?, ?, ?, ? ,?, ?)""",taqitem)
+                self.conn.commit()
         pass
     
     def onXTimerUpdate(self):        
         nowtime = time.localtime()
-        print nowtime
+        if nowtime.tm_hour == 9 and nowtime.tm_min > 23 and nowtime.tm_min < 26 and self.entry_counter1 < 10:
+
+            buysell = False
+            shcode = self.callShCode
+            price = 0.40
+            qty = 1
+
+            self.cur.execute("""SELECT Bid1 From options WHERE ShCode = ? ORDER BY TIME LIMIT 1""",(shcode,))
+            row = self.cur.fetchone()
+            price = float(row[0])
+
+            self.sendOrder(buysell,shcode,price,qty)
+
+            buysell = False
+            shcode = self.putShCode
+            price = 0.40
+            qty = 1
+
+            self.cur.execute("""SELECT Bid1 From options WHERE ShCode = ? ORDER BY TIME LIMIT 1""",(shcode,))
+            row = self.cur.fetchone()
+            price = float(row[0])
+
+            if self.sendOrder(buysell,shcode,price,qty):
+                self.entry_counter1+=1
+
+        elif nowtime.tm_hour == 11 and nowtime.tm_min > 23 and nowtime.tm_min < 26 and self.entry_counter2 < 10:
+            buysell = False
+            shcode = self.callShCode
+            price = 0.40
+            qty = 1
+
+            self.cur.execute("""SELECT Bid1 From options WHERE ShCode = ? ORDER BY TIME LIMIT 1""",(shcode,))
+            row = self.cur.fetchone()
+            price = float(row[0])
+
+            self.sendOrder(buysell,shcode,price,qty)
+
+            buysell = False
+            shcode = self.putShCode
+            price = 0.40
+            qty = 1
+
+            self.cur.execute("""SELECT Bid1 From options WHERE ShCode = ? ORDER BY TIME LIMIT 1""",(shcode,))
+            row = self.cur.fetchone()
+            price = float(row[0])
+
+            if self.sendOrder(buysell,shcode,price,qty):
+                self.entry_counter2+=1
+
+        elif nowtime.tm_hour == 14 and nowtime.tm_min > 42 and nowtime.tm_min < 45 and self.counter < 20:
+            #time.sleep(random.randint(0,2000) * 0.001)
+            buysell = True
+            shcode = self.callShCode
+            price = 1.90
+            qty = 1
+
+            self.cur.execute("""SELECT Ask1 From options WHERE ShCode = ? ORDER BY TIME LIMIT 1""",(shcode,))
+            row = self.cur.fetchone()
+            price = float(row[0])
+
+            self.sendOrder(buysell,shcode,price,qty)
+
+            buysell = True
+            shcode = self.putShCode
+            price = 1.90
+            qty = 1
+
+            self.cur.execute("""SELECT Ask1 From options WHERE ShCode = ? ORDER BY TIME LIMIT 1""",(shcode,))
+            row = self.cur.fetchone()
+            price = float(row[0])
+
+            if self.sendOrder(buysell,shcode,price,qty):
+                self.counter+=1
+
         pass
-    
+
+    def sendOrder(self,buysell,shcode,price,qty):
+        if type(self.socket).__name__ == 'Socket':
+            msg = str(buysell) + ',' + str(shcode) + ',' + str(price) + ',' + str(qty)
+            print msg
+            #self.socket.send(msg)
+            #msg_in = self.socket.recv()
+            #print msg_in
+            return True
+        else:
+            print 'not define socket..'
+            return False
+        pass
+
 if __name__ == '__main__':
     import sys
     app = QtGui.QApplication(sys.argv)
