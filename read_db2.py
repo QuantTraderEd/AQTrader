c390a8918be7119c0831d34b627ee5eb337f9afe
QuantTraderEd <hyojkim79@gmail.com>
2014-11-17 21:42:02 +0900

    Update read_db2.py
    change faster

--- read_db2.py
+++ read_db2.py
@@ -13,11 +13,12 @@ FeedCodeList_inst = FeedCodeList()
 FeedCodeList_inst.ReadCodeListFile()
 
 
-conn = lite.connect('TAQ_20141112.db')
+conn = lite.connect('TAQ_20141117.db')
+
 
 #cursor = conn.cursor()
 #cursor.execute("""SELECT * FROM FutOptTickData WHERE Time > '15:14:59.000' """)
-#cursor.execute("""Select OrdNo, ExecNo, ShortCD,UnExecQty From OrderList 
+#cursor.execute("""Select OrdNo, ExecNo, ShortCD,UnExecQty From OrderList
 #                                    WHERE OrdNo = ? and ExecNo is null """,('3640',))
 
 #col_names = [cn[0] for cn in cursor.description]
@@ -26,7 +27,7 @@ conn = lite.connect('TAQ_20141112.db')
 #SELECT Time, ShortCD, AskQty2, AskQty1, Ask1, Bid1, BidQty1, BidQty2
 #SELECT Time, ShortCD, Bid1, Ask1, BidQty1, AskQty1, Bid2, Ask2, BidQty2, AskQty2, Bid3, Ask3, BidQty3, AskQty3
 
-sqltext = """ 
+sqltext = """
 SELECT Time, ShortCD, TAQ, LastPrice, LastQty, BuySell
 FROM FutOptTickData
 WHERE TAQ IN ('E')
@@ -38,28 +39,30 @@ target_min = 9999
 target_cd = ''
 target_price = '1.5'
 
+
+sqltext = """
+SELECT Time, ShortCD, TAQ, LastPrice, LastQty, BuySell
+FROM FutOptTickData
+WHERE TAQ IN ('E')
+and Time between '08:58:00.000' and '08:59:30.000'
+and SUBSTR(ShortCD,1,3) = '201'
+Order by Time DESC
+"""
+
+
+df = pd.read_sql(sqltext, conn)
+
 for shortcd in FeedCodeList_inst.optionshcodelst:
-    sqltext = """
-    SELECT Time, ShortCD, TAQ, LastPrice, LastQty, BuySell
-    FROM FutOptTickData
-    WHERE TAQ IN ('E')
-    and Time between '08:55:00.000' and '08:59:30.000'
-    and ShortCD = '%s'
-    and SUBSTR(ShortCD,1,3) = '201'
-    Order by Time DESC
-    """%shortcd
-
-    df = pd.read_sql(sqltext, conn)
-
-    #print shortcd, len(df)
-    if len(df) > 0:
-        row = df.irow(0)
+    df_buffer = df[df['ShortCD'] == shortcd]
+    if len(df_buffer) > 0:
+        row = df_buffer.irow(-1)
         print row['ShortCD'], row['Time'],row['LastPrice']
         diff = abs(float(row['LastPrice']) - 1.5)
         if diff < target_min:
             target_min = diff
             target_cd = shortcd
             target_price = row['LastPrice']
+
 
 print target_cd, target_price
 
@@ -74,12 +77,13 @@ print target_cd, target_price
 #for row in rows:
 #    #print row
 #    print "%2s %-25s %-7s %-8s %-7s %-3s" % row
-    
+
 #cursor.execute("UPDATE OrderList SET Qty = ? WHERE ID = ?",('10',1))
 #conn.commit()
 
 #orderitem = ('2013-10-29 09:00:01.789','True','A069500','26950','10')
-#cursor.execute("INSERT INTO OrderList(Time,BuySell,ShortCD,Price,Qty) VALUES(?, ?, ?, ? ,?)",orderitem)            
+#cursor.execute("INSERT INTO OrderList(Time,BuySell,ShortCD,Price,Qty) VALUES(?, ?, ?, ? ,?)",orderitem)
 #conn.commit()
 
 conn.close()
+
