cc90cf3d0405462d979d2651a3916473a616160b
QuantTraderEd <hyojkim79@gmail.com>
2014-11-26 23:44:59 +0900

    Update read_db3.py
    Add more research process

--- read_db3.py
+++ read_db3.py
@@ -5,10 +5,11 @@ Created on Fri Nov 22 23:00:00 2014
 @author: assa
 """
 
+import pylab
 import sqlite3 as lite
 import pandas as pd
 
-conn = lite.connect('./Data/TAQ_20141120.db')
+conn = lite.connect('./SQLite/Data/TAQ_20141120.db')
 
 
 sqltext = """
@@ -70,6 +71,8 @@ sqltext = """
 SELECT ShortCD, FeedSource, TAQ, SecuritiesType, Time, AskQty1, Ask1, Bid1, BidQty1
 FROM FutOptTickData
 WHERE TAQ = 'Q'
+AND Time Between '09:00:00.000' and '10:00:00.000'
+AND Bid1 > '0.00'
 AND ShortCD IN ('201JC250','301JC250')
 Order by Time
 """
@@ -79,3 +82,56 @@ df = pd.read_sql(sqltext, conn)
 df_call = df[df['ShortCD']=='201JC250']
 df_put = df[df['ShortCD']=='301JC250']
 
+df_mid = pd.merge(df_call,df_put,on='Time',how='outer')
+df_mid = df_mid.fillna(method='pad')
+
+df_mid = df_mid[['Time','ShortCD_x','AskQty1_x','Ask1_x','Bid1_x','BidQty1_x','ShortCD_y','AskQty1_y','Ask1_y','Bid1_y','BidQty1_y']]
+df_mid = df_mid[df_mid['Bid1_y'].notnull()]
+
+
+df_mid[['Ask1_x','Bid1_x','Ask1_y','Bid1_y']] = df_mid[['Ask1_x','Bid1_x','Ask1_y','Bid1_y']].astype(float)
+df_mid['Syth_Bid'] = 250 + df_mid['Bid1_x'] - df_mid['Ask1_y']
+df_mid['Syth_Ask'] = 250 + df_mid['Ask1_x'] - df_mid['Bid1_y']
+
+
+#df_mid[['Time','Syth_Bid','Syth_Ask']].plot(x=df_mid['Time'])
+#matplotlib.show()
+
+
+sqltext = """
+SELECT ShortCD, FeedSource, TAQ, SecuritiesType, Time, AskQty1, Ask1, Bid1, BidQty1
+FROM FutOptTickData
+WHERE TAQ = 'Q'
+AND Time Between '09:00:00.000' and '10:00:00.000'
+AND Bid1 > '0.00'
+AND ShortCD IN ('201JC252','301JC252')
+Order by Time
+"""
+
+df = pd.read_sql(sqltext, conn)
+
+df_call = df[df['ShortCD']=='201JC252']
+df_put = df[df['ShortCD']=='301JC252']
+
+df_up = pd.merge(df_call,df_put,on='Time',how='outer')
+df_up = df_up.fillna(method='pad')
+
+df_up = df_up[['Time','ShortCD_x','AskQty1_x','Ask1_x','Bid1_x','BidQty1_x','ShortCD_y','AskQty1_y','Ask1_y','Bid1_y','BidQty1_y']]
+df_up = df_up[df_up['Bid1_y'].notnull()]
+
+
+df_up[['Ask1_x','Bid1_x','Ask1_y','Bid1_y']] = df_up[['Ask1_x','Bid1_x','Ask1_y','Bid1_y']].astype(float)
+df_up['Syth_Bid'] = 252 + df_up['Bid1_x'] - df_up['Ask1_y']
+df_up['Syth_Ask'] = 252 + df_up['Ask1_x'] - df_up['Bid1_y']
+
+
+#df_up[['Time','Syth_Bid','Syth_Ask']].plot(x=df_up['Time'])
+#matplotlib.show()
+
+
+df_syth = pd.merge(df_mid[['Time','Syth_Ask','Syth_Bid']],df_up[['Time','Syth_Ask','Syth_Bid']],on='Time',how='outer')
+df_syth = df_syth.fillna(method='pad')
+df_syth = df_syth[df_syth['Syth_Ask_y'].notnull()]
+
+df_syth['Arb1'] = df_syth['Syth_Bid_x'] - df_syth['Syth_Ask_y']
+df_syth['Arb2'] = df_syth['Syth_Bid_y'] - df_syth['Syth_Ask_x']
