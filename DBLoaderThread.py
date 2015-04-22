1975b4c82e471bc3552abd65f4d0af1edddc4e0b
QuantTraderEd <hyojkim79@gmail.com>
2015-04-22 23:17:15 +0900

    Update DBLoaderThread.py
    Add & correct msg part

--- DBLoaderThread.py
+++ DBLoaderThread.py
@@ -20,7 +20,9 @@ class DBLoaderThread(SubscribeThread):
     def __init__(self, parent=None, subtype='BackTest'):
         SubscribeThread.__init__(self, parent, subtype=subtype)
         self.count = 0
+        self.count_remain = 10
         self.Id_tag = -1
+        self.time_chk = True
         self.initDB()
         pass
 
@@ -42,10 +44,12 @@ class DBLoaderThread(SubscribeThread):
         else:
             self.MsgNotify.emit('loading from file DB to memory DB...')
             self.conn_file = lite.connect(self.strdbname, check_same_thread=False)
-            df = pd.read_sql("""SELECT * From FutOptTickData""",self.conn_file)
+            df = pd.read_sql("""SELECT * From FutOptTickData""", self.conn_file)
             pd.io.sql.write_frame(df, "FutOptTickData", self.conn_memory,'sqlite','replace')
             self.count = int(df['Id'].irow(-1))
             self.Id_tag = df['Id'].irow(-1)
+            self.count_remain = self.count % 1000 + 10
+            self.MsgNotify.emit('Start Count: ' + str(self.count))
         pass
 
     def initMemoryDB(self):
@@ -113,7 +117,7 @@ class DBLoaderThread(SubscribeThread):
         self.conn_file.commit()
         pass
 
-    def onReceiveData(self,msg):
+    def onReceiveData(self, msg):
         chk = ''
         taqitem = ()
         if type(msg) == str:            
@@ -125,7 +129,6 @@ class DBLoaderThread(SubscribeThread):
             else:
                 nightshift = 1            
             taqitem = parser.msgParser(msg, nightshift, tuple, self.count)
-            print taqitem
         elif type(msg) == dict:
             row = msg.copy()
             chk = row['TAQ']
@@ -181,14 +184,15 @@ class DBLoaderThread(SubscribeThread):
 
         if chk != '':
             try:
+                if not nightshift: print taqitem
                 self.conn_memory.execute(sqltext, taqitem)
                 self.conn_memory.commit()
-                self.count += 1                
-                if self.count % 1000 == 10:
+                if self.count % 1000 == self.count_remain:
                     cursor = self.conn_memory.execute("""SELECT COUNT(*) FROM FutOptTickData""")
                     row = cursor.fetchone()
                     msg = 'Count: %d' % row[0]
                     self.MsgNotify.emit(msg)
+                self.count += 1
             except lite.Error as e:
                 self.MsgNotify.emit(e.args[0])
 
@@ -202,17 +206,18 @@ class DBLoaderThread(SubscribeThread):
 
     def onXTimerUpdate(self):
         if os.path.isfile(self.strdbname):
-            # print 'read memomry db...'
+            self.MsgNotify.emit('replicate memory db to file db, %d, %d' % (self.Id_tag, self.count))
             try:
-                df_memory = pd.read_sql("""SELECT * From FutOptTickData WHERE Id > %d """ % self.Id_tag,self.conn_memory)
+                df_memory = pd.read_sql("""SELECT * From FutOptTickData WHERE Id > %d """ % self.Id_tag, self.conn_memory)
                 self.Id_tag = df_memory['Id'].irow(-1)
+                pd.io.sql.write_frame(df_memory, "FutOptTickData", self.conn_file, 'sqlite', 'append')
             except lite.Error as e:
                 msg = "An error occurred:", e.args[0]
-                self.MsgNotify(msg)
+                self.MsgNotify.emit(msg)
                 return
             # print 'test: write file db...'
             # df_memory = df_memory.iloc[:,1:len(df_memory.columns)]
-            pd.io.sql.write_frame(df_memory, "FutOptTickData", self.conn_file, 'sqlite', 'append')
+
 
         # else:
             # print 'make new file db'
@@ -220,6 +225,4 @@ class DBLoaderThread(SubscribeThread):
 
     def stop(self):        
         SubscribeThread.stop(self)
-        self.conn_memory.close()
-        self.conn_file.close()
         pass
