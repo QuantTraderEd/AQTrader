ccfa8b4ed65fe80235ce765d37f338e7db0b1b93
QuantTraderEd <hyojkim79@gmail.com>
2015-05-18 23:30:26 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 19 23:56:06 2015
+
+@author: assa
+"""
+
+import sys
+from cx_Freeze import setup, Executable
+
+base = None
+if sys.platform == 'win32':
+    base = 'Win32GUI'
+    
+executables = [Executable('DBWidget.py', base=base)]
+includes = ["zmq", "zmq.utils.garbage", "zmq.backend.cython"]
+setup(name='test',
+      version='0.1',
+      description='Test',
+      options={"build_exe": {"includes": includes}},
+      executables=executables
+)
