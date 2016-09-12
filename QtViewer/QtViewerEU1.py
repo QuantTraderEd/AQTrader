d2f776e168dba34114093889c590734ce6a313b4
QuantTraderEd <hyojkim79@gmail.com>
2016-09-12 21:00:23 +0900

    Update for add publish exec_report (msg_dict)

--- QtViewer/QtViewerEU1.py
+++ QtViewer/QtViewerEU1.py
@@ -16,11 +16,14 @@ def convert(strprice):
 
 class QtViewerEU1(QtCore.QObject):
     receive = QtCore.pyqtSignal()
-    def __init__(self,parent=None):
-        super(QtViewerEU1,self).__init__(parent)
+
+    def __init__(self, zmq_socket=None, parent=None):
+        super(QtViewerEU1, self).__init__(parent)
+        self.zmq_socket = zmq_socket
         self.dbname = None
         self.flag = True
         self.logger = logging.getLogger('ZeroOMS.Thread.EU1')
+        self.logger.info('init QtViewerEU1')
 
     def Update(self, subject):
         #print '-' * 20
@@ -30,6 +33,7 @@ class QtViewerEU1(QtCore.QObject):
             #print 'szMessage',  subject.data['szMessage']
             #print 'szMessageCode', subject.data['szMessageCode'],
             #print subject.data
+            ordno = subject.data['ordno']
             orgordno = subject.data['orgordno']
             execno = None
 
@@ -37,17 +41,35 @@ class QtViewerEU1(QtCore.QObject):
             elif subject.data['bnstp'] == '1': buysell = 'sell'
             else: buysell = None
 
-            shcode = subject.data['fnoIsuno']   # or isuno
+            shortcd = subject.data['fnoIsuno']   # or isuno
             ordprice = None
             ordqty = None
-            #ordprice = subject.data['ordprc']
-            #ordqty = subject.data['ordqty']
+            # ordprice = subject.data['ordprc']
+            # ordqty = subject.data['ordqty']
             execprice = subject.data['execprc']
             execqty = subject.data['execqty']
             unexecqty = subject.data['unercqty']
-            orderitem = (orgordno,strnowtime,buysell,shcode,ordprice,ordqty,execprice,execqty,unexecqty)
+            orderitem = (ordno, strnowtime, buysell, shortcd, ordprice, ordqty, execprice, execqty, unexecqty)
+
+            if buysell == 'buy': buysell = 'B'
+            elif buysell == 'sell': buysell = 'S'
+            else: buysell = ''
+
+            msg_dict = {}
+            msg_dict['OrdNo'] = ordno
+            msg_dict['ExecNo'] = execno
+            msg_dict['TimeStamp'] = nowtime
+            msg_dict['ShortCD'] = shortcd
+            msg_dict['OrdPrice'] = ordprice
+            msg_dict['OrdQty'] = ordqty
+            msg_dict['buysell'] = buysell
+            msg_dict['ExecPrice'] = execprice
+            msg_dict['ExecQty'] = execqty
+
+            self.zmq_socket.send_pyobj(msg_dict)
+
             self.logger.info(str(orderitem))
-            #print orderitem
+
             if self.dbname != None:
                 conn_db = lite.connect(self.dbname)
                 cursor_db = conn_db.cursor()
@@ -55,7 +77,7 @@ class QtViewerEU1(QtCore.QObject):
                                                 VALUES(?, ?, ?, ? ,?, ?, ?, ?, ?)""",orderitem)
 
                 cursor_db.execute("""Select ExecPrice,ExecQty,UnExecQty From OrderList
-                                    WHERE OrdNo = ? and ShortCD = ? and ChkReq is not null """,(str(orgordno),str(shcode),))
+                                    WHERE OrdNo = ? and ShortCD = ? and ChkReq is not null """,(str(ordno),str(shortcd),))
                 rows = cursor_db.fetchall()
                 unexecqty = 0
                 avgExecPrice = 0
@@ -76,7 +98,7 @@ class QtViewerEU1(QtCore.QObject):
                                                                     WHERE OrdNo=? and ShortCD = ? and (BuySell = 'buy' or BuySell = 'sell')
                                                                     and ChkReq is not null
                                                                     """,
-                                                                    (str(avgExecPrice), str(avgExecQty),str(unexecqty - int(execqty)),str(orgordno),str(shcode),))
+                                                                    (str(avgExecPrice), str(avgExecQty),str(unexecqty - int(execqty)),str(ordno),str(shortcd),))
                 conn_db.commit()
                 conn_db.close()
                 #self.emit(QtCore.SIGNAL("OnReceiveData (QString)"),'SC1')
