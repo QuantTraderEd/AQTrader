f90f06aceb8bc48963162bd6eb5861ef212e6254
QuantTraderEd <hyojkim79@gmail.com>
2016-11-07 23:31:22 +0900

    Update for data type dict

--- cybos_OptionCurOnly.py
+++ cybos_OptionCurOnly.py
@@ -1,23 +1,38 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Aug 27 13:02:06 2013
-
-@author: Administrator
-"""
 
 from cybos_source import Source
 
+
 class OptionCurOnly(Source):
-    """ subscribe option trade tick """
-    def __init__(self, code = None):
+    """
+    subscribe kospi200 option (mini) trade tick
+    """
+    def __init__(self, code=None, datatype='list'):
         super(OptionCurOnly, self).__init__('CpSysDib.OptionCurOnly.1')
         self.type = 'TAQ'
-        self.data = None
+        self.datatype = datatype
+        if self.datatype == 'list':
+            self.data = []
+        elif self.datatype == 'dict':
+            self.data = {}
+        else:
+            self.data = None
         if code: self.SetInputValue('0',code)
         pass
+
     def OnSignal(self):
-        self.data = []
-        for i in xrange(29): self.data.append(self.com.GetHeaderValue(i))
-        # for debug
-        self.Notify()
+        if self.datatype == 'list':
+            self.data = []
+            for i in xrange(29): self.data.append(self.com.GetHeaderValue(i))
+            self.Notify()
+        if self.datatype == 'dict':
+            self.data = {}
+            self.data['LastPrice'] = self.com.GetHeaderValue(2)
+            self.data['LastQty'] = 0
+            self.data['Volume'] = self.com.GetHeaderValue(7)
+            self.data['BuySell'] = self.com.GetHeaderValue(21)
+            self.data['Ask1'] = self.com.GetHeaderValue(17)
+            self.data['Bid1'] = self.com.GetHeaderValue(18)
+            self.data['ShortCD'] = self.com.GetHeaderValue(0)
+            self.Notify()
         pass
--- cybos_OptionJpBid.py
+++ cybos_OptionJpBid.py
@@ -1,22 +1,46 @@
 # -*- coding: utf-8 -*-
-"""
-Created on Tue Aug 27 13:09:47 2013
-
-@author: Administrator
-"""
 
 from cybos_source import Source
 
+
 class OptionJpBid(Source):
-    """ subscribe index option quote """
-    def __init__(self, code = None):
+    """
+    subscribe kospi200 index option (mini) quote
+    """
+    def __init__(self, code=None, datatype='list'):
         super(OptionJpBid, self).__init__('CpSysDib.OptionJpBid.1')
         self.type = 'TAQ'
-        self.data = None
+        self.datatype = datatype
+        if self.datatype == 'list':
+            self.data = []
+        elif self.datatype == 'dict':
+            self.data = {}
+        else:
+            self.data = None
         if code: self.SetInputValue('0',code)
         pass
+
     def OnSignal(self):
-        self.data = []
-        for i in xrange(37): self.data.append(self.com.GetHeaderValue(i))
-        self.Notify()
-    pass
+        if self.datatype == 'list':
+            self.data = []
+            for i in xrange(37): self.data.append(self.com.GetHeaderValue(i))
+            self.Notify()
+        elif self.datatype == 'dict':
+            self.data = {}
+            for i in xrange(1, 6):
+                self.data['Ask%d' % i] = self.com.GetHeaderValue(2 + i - 1)
+                self.data['Bid%d' % i] = self.com.GetHeaderValue(19 + i - 1)
+                self.data['AskQty%d' % i] = self.com.GetHeaderValue(7 + i - 1)
+                self.data['BidQty%d' % i] = self.com.GetHeaderValue(24 + i - 1)
+                self.data['AskCnt%d' % i] = self.com.GetHeaderValue(13 + i - 1)
+                self.data['BidCnt%d' % i] = self.com.GetHeaderValue(30 + i - 1)
+
+            self.data['TotalAskQty'] = self.com.GetHeaderValue(12)
+            self.data['TotalBidQty'] = self.com.GetHeaderValue(29)
+            self.data['TotalAskCnt'] = self.com.GetHeaderValue(18)
+            self.data['TotalBidCnt'] = self.com.GetHeaderValue(35)
+            self.data['ShortCD'] = self.com.GetHeaderValue(0)
+            self.Notify()
+
+        pass
+
