abd93a2c62e01dbff401d8ec9cbadd8cd9e34e58
QuantTraderEd <hyojkim79@gmail.com>
2018-08-19 00:43:35 +0900

    add setup.py

---
+++ PyCybos/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+import os
+from setuptools import setup, find_packages
+
+# Utility function to read the README file.
+# Used for the long_description.  It's nice, because now 1) we have a top level
+# README file and 2) it's easier to type in the README file than to put a raw
+# string in below ...
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+setup(
+    name="pycybos",
+    version="0.1",
+    author="QuantTraderEd",
+    description="Python package for Cybos",
+    url="https://github.com/QuantTraderEd/AQTrader/tree/master/PyCybos",
+    packages=['pycybos'],
+    long_description=open('README.md').read(),
+    # setup_requires=['pythoncom', 'win32com'],
+    # test_suite=''
+)
