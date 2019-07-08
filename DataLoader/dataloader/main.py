dfae64505543b065afc49426d239deb2d931cb30
QuantTraderEd <hyojkim79@gmail.com>
2019-07-08 23:22:19 +0900

    add run func initTIMER

--- DataLoader/dataloader/main.py
+++ DataLoader/dataloader/main.py
@@ -33,6 +33,7 @@ class MainForm(QtGui.QWidget):
     def __init__(self, parent=None):
         QtGui.QWidget.__init__(self, parent)
         self.initUI()
+        self.initTIMER()
         self.initThread()
         pass
 
