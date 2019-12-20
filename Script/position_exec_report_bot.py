042b9badc6b372ffa5a9a0c56559ef991c532182
QuantTraderEd <hyojkim79@gmail.com>
2019-12-20 23:06:51 +0900

    update position report for only new order

--- Script/position_exec_report_bot.py
+++ Script/position_exec_report_bot.py
@@ -15,7 +15,7 @@ context = zmq.Context()
 socket = context.socket(zmq.SUB)
 socket.connect("tcp://127.0.0.1:7001")      # demo
 # socket.connect("tcp://127.0.0.1:7000")      # real
-socket.setsockopt(zmq.SUBSCRIBE,"")
+socket.setsockopt(zmq.SUBSCRIBE, "")
 
 update_id = None
 chat_id = '49417214'
@@ -28,15 +28,15 @@ feedcode_list.read_code_list()
 futures_shortcd_lst = [feedcode_list.future_shortcd_list[0],
                        feedcode_list.future_shortcd_list[2]]
 position_dict = dict()
-autotrader_id = 'MiniArb001'
+target_autotrader_id = 'MiniArb001'
 
 
 def main():
     now_dt = dt.datetime.now()
     for shortcd in futures_shortcd_lst:
-        qty = redis_client.hget(autotrader_id + '_position_dict', shortcd)
+        qty = redis_client.hget(target_autotrader_id + '_position_dict', shortcd)
         # logger.info('%s positon-> %s' % (shortcd, qty))
-        avg_price = redis_client.hget(autotrader_id + '_tradeprice_dict', shortcd)
+        avg_price = redis_client.hget(target_autotrader_id + '_tradeprice_dict', shortcd)
         # logger.info('%s tradeprice-> %s' % (shortcd, avg_price))
         position_dict[shortcd] = [int(qty or 0), "%.3f" % float(avg_price or 0)]
 
@@ -47,18 +47,21 @@ def main():
 
     while True:
         msg_dict = socket.recv_pyobj()
+        autotrader_id = msg_dict['autotrader_id']
+        new_amend_cancel = msg_dict['new_amend_cancel']
         time.sleep(1)
-        now_dt = dt.datetime.now()
-        for shortcd in futures_shortcd_lst:
-            qty = redis_client.hget(autotrader_id + '_position_dict', shortcd)
-            # logger.info('%s positon-> %s' % (shortcd, qty))
-            avg_price = redis_client.hget(autotrader_id + '_tradeprice_dict', shortcd)
-            # logger.info('%s tradeprice-> %s' % (shortcd, avg_price))
-            position_dict[shortcd] = [int(qty or 0), "%.3f" % float(avg_price or 0)]
+        if new_amend_cancel == 'N':
+            now_dt = dt.datetime.now()
+            for shortcd in futures_shortcd_lst:
+                qty = redis_client.hget(target_autotrader_id + '_position_dict', shortcd)
+                # logger.info('%s positon-> %s' % (shortcd, qty))
+                avg_price = redis_client.hget(target_autotrader_id + '_tradeprice_dict', shortcd)
+                # logger.info('%s tradeprice-> %s' % (shortcd, avg_price))
+                position_dict[shortcd] = [int(qty or 0), "%.3f" % float(avg_price or 0)]
 
-        msg = pprint.pformat(position_dict)
-        bot.send_message(chat_id, msg)
-        print(now_dt, msg)
+            msg = pprint.pformat(position_dict)
+            bot.send_message(chat_id, msg)
+            print(now_dt, msg)
 
 
 if __name__ == "__main__":
